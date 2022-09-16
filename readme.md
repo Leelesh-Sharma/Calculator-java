7508252085763a1dbbeee62074c47140ad83c399
Leelesh Sharma <60009872+Leelesh-Sharma@users.noreply.github.com>
2022-09-16 19:17:47 +0530

    Initialized Frame and readme file

---
+++ readme.md
@@ -0,0 +1,2 @@
+# Calculator
+It is a simple project created using Java Swing.
---
+++ src/frame.java
@@ -0,0 +1,16 @@
+package src;
+
+
+import javax.swing.*;
+
+public class frame extends JFrame  // JFrame superclass hai
+{
+    
+    frame(){
+        this.setTitle("Calculator - Leelesh");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(600,600);
+        this.setResizable(false);
+        this.setVisible(true);
+    }
+}
